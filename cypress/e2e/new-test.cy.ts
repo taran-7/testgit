0f42a80850d4c818cbd8457ff39b6f0afdf40370
Oleksandr Taraniuk <oleksandr.taraniuk@spsoft.com>
2023-02-01 16:02:24 +0200

    up

---
+++ cypress/e2e/new-test.cy.ts
@@ -0,0 +1,24 @@
+/// <reference types="cypress" />
+
+describe('Three tests', () => {
+
+    it('Playwright test', () => {
+
+        //   cy.visit('https://playwright.dev/');
+        //   cy.contains('Get started').click();
+        //   cy.wait(2000);
+
+    })
+
+    it('Vega.ua test', () => {
+
+        //   cy.visit('https://vega.ua/ukr/for_home');
+        //   cy.get('[class="shadowed"]').last().click();
+        //   cy.wait(2000);
+
+
+    })
+
+
+
+})
